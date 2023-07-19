ccdf16b849c9363a9fd07ff4d6325af67a6d93eb
Memorizu <kaidohmary@gmail.com>
2023-07-19 10:51:26 +0300

    make init migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название категории')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=20, decimal_places=2, verbose_name='Цена за покупку')
+    create_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    last_change_date = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} - {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+password = os.getenv('PASSWORD_POSTGRES')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,8 +91,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goods',
+        'USER': 'postgres',
+        'PASSWORD': password,
     }
 }
 
@@ -118,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -130,7 +133,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
